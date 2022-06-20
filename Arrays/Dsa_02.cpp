616e6d62fced72434c16d3012d75cceb0a2a7185
Pratham Katariya <68104206+prathamkatariya@users.noreply.github.com>
2022-06-20 12:28:49 +0530

    Update Dsa_02.cpp

--- Arrays/Dsa_02.cpp
+++ Arrays/Dsa_02.cpp
@@ -1,3 +1,5 @@
+// Finding the minimum and maximum element in an array.
+
 #include<iostream>
 #include<climits>
 
@@ -5,22 +7,22 @@ using namespace std;
 
 int main(){
 
-    int n;
+    int n;                          // Taking input of size of an array.
     cin>>n;
     int arr[n];
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)     // Taking Array input.
     {
         cin>>arr[i];
     }
 
-    int max = INT_MIN;
-    int min = INT_MAX;
+    int max = INT_MIN;              // Giving the minimum value to the max variable.
+    int min = INT_MAX;              // Giving the maximum value to the min variable.
 
     for (int i = 0; i < n; i++)
     {
-        if (arr[i]>max)
-        {
-            max = arr[i];
+        if (arr[i]>max)             // Checking every element that it 
+        {                           // is greater than max or not if yes
+            max = arr[i];           // then update max.
         }
         
     }
@@ -28,9 +30,9 @@ int main(){
 
       for (int i = 0; i < n; i++)
     {
-        if (arr[i]<min)
-        {
-            min = arr[i];
+        if (arr[i]<min)             // Checking every element that it
+        {                           // is smaller than min or not if yes
+            min = arr[i];           // then update min.
         }
         
     }
@@ -39,3 +41,5 @@ int main(){
     
     return 0;
 }
+
+// Time Complexity : O(n).
