4e1cf17b9d658182f3604ffe91676921ee41ead9
Pratham Katariya <68104206+prathamkatariya@users.noreply.github.com>
2022-06-20 13:12:09 +0530

    Update Dsa_05.cpp

--- Arrays/Dsa_05.cpp
+++ Arrays/Dsa_05.cpp
@@ -1,3 +1,6 @@
+// Move all the negative elements to one side of the array .
+
+
 #include <iostream>
 
 using namespace std;
@@ -6,29 +9,31 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
+        int n;                      // Taking input of size of an array.
         cin>>n;
         int a[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)     // Taking Array input.
         {
             cin>>a[i];
         }
 
-        int j = 0;
+        int j = 0;              // Giving j the first position.
 
         for (int i = 0; i < n; i++)
         {
             if(a[i]<0){
-                swap(a[i],a[j]);
-                j++;
+                swap(a[i],a[j]);    // By Swaping all negative numbers 
+                j++;                // are on the left side of an array.
             }
         }
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) 
         {
-            cout<<a[i]<<" ";
+            cout<<a[i]<<" ";        // output the result.
         }
         
     }
     return 0;
 }
+
+// Time Complexity : O(n).
