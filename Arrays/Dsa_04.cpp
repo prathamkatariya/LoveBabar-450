62aaa826eeb89ca6023d37597db5c8725e782033
Pratham Katariya <68104206+prathamkatariya@users.noreply.github.com>
2022-06-20 13:03:35 +0530

    Update Dsa_04.cpp

--- Arrays/Dsa_04.cpp
+++ Arrays/Dsa_04.cpp
@@ -1,3 +1,7 @@
+// Given an array which consists of only 0, 1 and 2. Sort the array 
+// without using any sorting algo.
+
+
 #include <iostream>
 
 using namespace std;
@@ -6,25 +10,25 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
+        int n;          // Taking input of size of an array.
         cin>>n;
-        int a[n];
-        for(int i=0;i<n;i++){
+        int a[n];       
+        for(int i=0;i<n;i++){       // Taking Array input.
             cin>>a[i];
         }
-        int count_1 = 0;
+        int count_1 = 0;            // Initialize variables with 0.
         int count_2 = 0;
         int count_3 = 0;
 
         for (int i = 0; i < n; i++)
         {
-            if(a[i]==0){
+            if(a[i]==0){            // Counting no. of 0's.
                 count_1++;
             }
-            else if(a[i]==1){
+            else if(a[i]==1){       // Counting no. of 1's.
                 count_2++;
             }
-            else{
+            else{                   // Counting no. of 2's.
                 count_3++;
             }
         }
@@ -32,20 +36,23 @@ int main(){
         int k =0;
         for (int i = 0; i < count_1 ; i++)
         {
-            a[k++] = 0;
+            a[k] = 0;
+            k++;
         }
-        for (int i = 0; i < count_2 ; i++)
+        for (int i = 0; i < count_2 ; i++)   // updating the array.
         {
-            a[k++] = 1;
+            a[k] = 1;
+            k++;
         }
         for (int i = 0; i < count_3 ; i++)
         {
-            a[k++] = 2;
+            a[k] = 2;
+            k++;
         }
         
         for (int i = 0; i < n ; i++)
         {
-            cout<<a[i]<<" ";
+            cout<<a[i]<<" ";             // Printing the output.
         }
         
     }
@@ -53,3 +60,5 @@ int main(){
 
     return 0;
 }
+
+// Time Complexity : O(n).
